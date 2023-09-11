5d2430b8d6a401bdf27526de468972010589783b
Daniel-0507 <davdiu20@epoka.edu.al>
2023-09-11 04:41:04 +0200

    changes

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Notes</h1>
+
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Note</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
